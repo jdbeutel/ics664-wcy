43710de9c196602b2abdb038289ab7f006eb034e
J. David Beutel <git@getsu.com>
2010-04-10 23:56:38 -1000

    hello world

---
+++ grails-app/controllers/com/getsu/SettingsController.groovy
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2010 J. David Beutel <software@getsu.com>
+ *
+ * Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
+ */
+
+package com.getsu
+
+class SettingsController {
+
+    def index = {
+        render "Hello Grails world!"
+    }
+}
---
+++ test/unit/com/getsu/SettingsControllerTests.groovy
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2010 J. David Beutel <software@getsu.com>
+ *
+ * Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
+ */
+
+package com.getsu
+
+import grails.test.*
+
+class SettingsControllerTests extends ControllerUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testHelloWorld() {
+        controller.index()
+        assertEquals "Hello Grails world!", controller.response.contentAsString
+    }
+}
